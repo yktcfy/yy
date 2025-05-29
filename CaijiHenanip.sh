ba26b9a6d6a8ab5d18663751e8b102a317fadc05
yktcfy <zjyktcfy@qq.com>
2025-05-29 17:06:56 +0800

    Update CaijiHenanip.sh

--- CaijiHenanip.sh
+++ CaijiHenanip.sh
@@ -3,7 +3,7 @@ if [ $# -eq 0 ]; then
   echo "开始测试······"
   echo "在5秒内输入1~4可选择城市"
   echo "请选择城市："
-  echo "1. 河南（Henan_liantong）"
+  echo "1. 河南（Henan_dianxin）"
   read -t 5 -p "超时未输入,将按默认设置测试" city_choice
 
   if [ -z "$city_choice" ]; then
@@ -17,8 +17,8 @@ fi
 # 设置城市和相应的stream
 case $city_choice in
     1)
-        city="Henan_liantong"
-        stream="rtp/225.1.4.55:1084"
+        city="Henan_dianxin"
+        stream="rtp/239.16.20.167:11670"
         ;;
     0)
         # 逐个处理{ }内每个选项
