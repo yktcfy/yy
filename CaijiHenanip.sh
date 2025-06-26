4c7c93d37fc6a779d5eadad0827b54e235d3263c
yktcfy <zjyktcfy@qq.com>
2025-06-26 12:07:47 +0800

    Update CaijiHenanip.sh

--- CaijiHenanip.sh
+++ CaijiHenanip.sh
@@ -18,7 +18,7 @@ fi
 case $city_choice in
     1)
         city="Henan_dianxin"
-        stream="rtp/239.11.0.108:5140"
+        stream="rtp/225.1.8.82:8000"
         ;;
     0)
         # 逐个处理{ }内每个选项
